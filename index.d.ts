ab42b4151828e0fae1c8706afbe04b399d4595c1
Kravchenko Oleksandr <madcap99@gmail.com>
2021-09-07 21:21:14 +0700

    add typing

--- index.d.ts
+++ index.d.ts
@@ -9,214 +9,213 @@ declare module 'laravel-js-str' {
         /**
          * Return the remainder of a string after the first occurrence of a given value
          */
-        after(search?): Stringable
+        public after(search?): Stringable
 
         /**
          * Returns the remainder of a string after the last occurrence of a given value
          */
-        afterLast(subject, search?: string): Stringable
+        public afterLast(subject, search?: string): Stringable
 
         /**
          * Append the given values to the string.
          * @return Stringable
          */
-        append(...values): Stringable
+        public append(...values): Stringable
 
         /**
          * Transliterate a UTF-8 value to ASCII.
          */
-        ascii(language?: string): Stringable
+        public ascii(language?: string): Stringable
 
         /**
          * Get the portion of a string before the first occurrence of a given value
          *
          */
-        before(search?: string): Stringable
+        public before(search?: string): Stringable
 
         /**
          * Get the portion of a string before the last occurrence of a given value
          */
-        beforeLast(search?: string): Stringable
+        public beforeLast(search?: string): Stringable
 
         /**
          * Get the portion of a string between two given values
          */
-        between(at?: string, to?: string): Stringable
+        public between(at?: string, to?: string): Stringable
 
         /**
          * Convert a value to camel case.
          */
-        camel(): Stringable
+        public camel(): Stringable
 
         /**
          * Determine if a given string contains a given substring.
          * @return boolean
          */
-        contains(needles?: Array<any> | string): boolean
+        public contains(needles?: Array<any> | string): boolean
 
         /**
          * Determine if a given string contains all array values
          */
-        containsAll(needles?: Array<any>): boolean
+        public containsAll(needles?: Array<any>): boolean
 
         /**
          * Determine if a given string ends with a given substring
          */
-        endsWith(needles?: Array<any> | string): boolean
+        public endsWith(needles?: Array<any> | string): boolean
 
 
         /**
          * Determine if the string is an exact match with the given value.
          */
-        exactly(value): boolean
+        public exactly(value): boolean
 
         /**
          * Explode the string into an array.
          */
-        explode(delimiter, limit: number): typeof Collection
+        public explode(delimiter, limit: number): typeof Collection
 
         /**
          * Split a string using a regular expression.
          */
-        split(pattern, limit?: number, flags?: number): typeof Collection;
+        public split(pattern, limit?: number, flags?: number): typeof Collection;
 
         /**
          * Cap a string with a single instance of a given value
          */
-        finish(cap): Stringable
+        public finish(cap): Stringable
 
         /**
          * Determine if a given string matches a given pattern
          */
-        is(pattern): boolean
+        public is(pattern): boolean
 
         /**
          * Determine if a given string is 7 bit ASCII
          */
-        isAscii(): boolean
+        public isAscii(): boolean
 
         /**
          * Determine if the given string is empty.
          */
-        isEmpty(): boolean
+        public isEmpty(): boolean
 
         /**
          * Determine if the given string is empty.
          */
-        isNotEmpty(): boolean
-
+        public isNotEmpty(): boolean
 
         /**
          * Convert a string to kebab case.
          */
-        kebab(): Stringable
+        public kebab(): Stringable
 
         /**
          * Return the length of the given string
          */
-        length(encoding?): number
+        public length(encoding?): number
 
         /**
          * Limit the number of characters in a string.
          *
          * @return Stringable
          */
-        limit(limit?: number, end?: string): Stringable
+        public limit(limit?: number, end?: string): Stringable
 
         /**
          * Convert the given string to lower-case
          */
-        lower(): Stringable
+        public lower(): Stringable
 
         /**
          * Get the string matching the given pattern.
          * @return Stringable|null
          */
-        match(pattern): Stringable | null;
+        public match(pattern): Stringable | null;
 
         /**
          * Parse a Class@method style callback into class and method
          */
-        parseCallback(fallback): Array<string>
+        public parseCallback(fallback): Array<string>
 
         /**
          * Get the plural form of an English word.
          */
-        plural(count: number): Stringable
+        public plural(count: number): Stringable
 
         /**
          * Pluralize the last word of an English, studly caps case string.
          */
-        pluralStudly(count: number): Stringable
+        public pluralStudly(count: number): Stringable
 
         /**
          * Replace the given value in the given string.
          *
          * @return Stringable
          */
-        replace(search, replace): Stringable
+        public replace(search, replace): Stringable
 
         /**
          * Replace a given value in the string sequentially with an array.
          * @return Stringable
          */
-        replaceArray(search, replace: Array<string>): Stringable
+        public replaceArray(search, replace: Array<string>): Stringable
 
         /**
          * Replace the first occurrence of a given value in the string.
          */
-        replaceFirst(search, replace): Stringable
+        public replaceFirst(search, replace): Stringable
 
         /**
          * Replace the last occurrence of a given value in the string.
          */
-        replaceLast(search, replace): Stringable
+        public replaceLast(search, replace): Stringable
 
         /**
          * Begin a string with a single instance of a given value
          *
          */
-        start(prefix): Stringable
+        public start(prefix): Stringable
 
         /**
          * Convert the given string to upper-case.
          */
-        upper(): Stringable
+        public upper(): Stringable
 
         /**
          * Convert the given string to title case.
          */
-        title(): Stringable
+        public title(): Stringable
 
         /**
          * Get the singular form of an English word.
          */
-        singular(): Stringable
+        public singular(): Stringable
 
         /**
          *
          */
-        slug(separator?, language?): Stringable
+        public slug(separator?, language?): Stringable
 
         /**
          * Convert a string to snake case.
          */
-        snake(delimiter?): Stringable
+        public snake(delimiter?): Stringable
 
         /**
          * Determine if a given string starts with a given substring.
          */
-        startsWith(needles): Stringable
+        public startsWith(needles): Stringable
 
         /**
          * Convert a value to studly caps case.*
          */
-        studly(): Stringable
+        public studly(): Stringable
 
         /**
          * Returns the portion of string specified by the start and length parameters.
          */
-        substr(start, length?): Stringable
+        public substr(start, length?): Stringable
 
         /**
          * Returns the number of substring occurrences.
@@ -227,72 +226,72 @@ declare module 'laravel-js-str' {
          *
          * @return int
          */
-        substrCount(needle, offset?, length?): number;
+        public substrCount(needle, offset?, length?): number;
 
         /**
          * Trim the string of the given characters.
          *
          */
-        trim(characters?): Stringable;
+        public trim(characters?): Stringable;
 
         /**
          * Left trim the string of given characters.
          */
-        ltrim(characters?): Stringable
+        public ltrim(characters?): Stringable
 
 
         /**
          * Right trim the string of given characters.
          */
-        rtrim(characters?): Stringable
+        public rtrim(characters?): Stringable
 
         /**
          * Make a string's first character uppercase.
          */
-        whenEmpty(callback: Function): Stringable
+        public whenEmpty(callback: Function): Stringable
 
         /**
          * Limit the number of words in a string.
          */
-        words(words?, end?): Stringable
+        public words(words?, end?): Stringable
 
         /**
          * Dump the string.
          */
-        dump(): this
+        public dump(): this
 
         /**
          * Dump the string then die.
          */
-        dd(): void
+        public dd(): void
 
         /**
          * Proxy dynamic properties properties onto methods.*
          */
-        get(key): any
+        public get(key): any
 
         /**
          * Used when returning as a string operation
          */
-        toLocaleString(): string
+        public toLocaleString(): string
 
         /**
          * Used when returning as a string operation
          *
          * @returns string
          */
-        toString(): string
+        public toString(): string
 
         /**
          * Used when JSON.stringify is called
          */
-        toJSON()
+        public toJSON()
 
         /**
          * Used when returning as a value operation
          *
          */
-        valueOf()
+        public valueOf()
     }
 
     export class Str {
